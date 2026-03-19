596548a0f14858bc34cf44d2b4d145ec7e63bc80
Mallow <mencheliu@gmail.com>
2026-03-19 19:52:10 +0800

    updates

--- project foodie/src/main.js
+++ project foodie/src/main.js
@@ -3,10 +3,9 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router/router";
 import { createPinia } from "pinia";
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
-
 
 createApp(App).use(pinia).use(router).mount("#app");
