7e2159634c3b917a680be811b950a4396eb9076e
Mallow <mencheliu@gmail.com>
2026-04-03 14:31:44 +0800

    aside positioning fin

--- src/components/map_aside.vue
+++ src/components/map_aside.vue
@@ -24,81 +24,108 @@ const toggleAside = () => {
 
 <template>
   <aside :class="{ 'aside-expanded': isExpanded }" class="aside">
-    <div :class="{ 'content-toggle': isExpanded }" class="aside-content">
-      <div class="aside-row">
-        <BackBtn class="back-btn"></BackBtn>
-        <form action="" class="search-bar">
-          <i class="fa-solid fa-magnifying-glass"></i>
-          <input type="text" class="search-input" placeholder="請輸入關鍵字" />
-        </form>
-        <button class="advance-search">
-          <i class="fa-solid fa-filter"></i>
-          <span>篩選</span>
-        </button>
-      </div>
+    <div class="scroll-container">
+      <div :class="{ 'content-toggle': isExpanded }" class="aside-content">
+        <div class="aside-row">
+          <div class="row-wrapper">
+            <BackBtn class="back-btn"></BackBtn>
+            <form action="" class="search-bar">
+              <i class="fa-solid fa-magnifying-glass"></i>
+              <input
+                type="text"
+                class="search-input"
+                placeholder="請輸入關鍵字"
+              />
+            </form>
+            <button class="advance-search">
+              <i class="fa-solid fa-filter"></i>
+              <span>篩選</span>
+            </button>
+          </div>
+        </div>
 
-      <div v-if="!store.selectedRestaurant" class="res-list">
-        <p class="result-count">共 {{ props.restaurants.length }} 家餐廳</p>
-        <ol>
-          <li
-            v-for="res in props.restaurants"
-            :key="res.id"
-            class="list-card"
-            @click="store.setSelectedRestaurant(res)"
-          >
-            <img
-              v-if="res.image"
-              :src="res.image"
-              alt="餐廳圖片"
-              class="card-img"
-            />
-            <div class="card-info">
-              <h3 class="card-name">{{ res.name }}</h3>
-              <div class="card-data">
-                <div>
+        <div class="res-list">
+          <!-- <p class="result-count">共 {{ props.restaurants.length }} 家餐廳</p> -->
+          <ol>
+            <li
+              v-for="res in props.restaurants"
+              :key="res.id"
+              class="list-card"
+              @click="store.setSelectedRestaurant(res)"
+            >
+              <img
+                v-if="res.image"
+                :src="res.image"
+                alt="餐廳圖片"
+                class="card-img"
+              />
+              <div class="card-info">
+                <h3 class="card-name">{{ res.name }}</h3>
+                <div class="card-data">
                   <Stars :rating="res.rating" :reviewCount="res.reviewCount" />
-                  | {{ res.priceRange }}
+                  <p>{{ res.priceRange }}</p>
+                  <p>{{ res.address }}</p>
                 </div>
-                <p>{{ res.address }}</p>
               </div>
-            </div>
-          </li>
-        </ol>
-      </div>
-
-      <div v-else class="detail-container">
-        <button @click="store.clearSelectedRestaurant()">
-          <i class="fa-solid fa-arrow-left"></i>
-        </button>
-        <img
-          :src="store.selectedRestaurant.image"
-          alt="餐廳圖片"
-          class="detail-res-image"
-        />
-        <div class="detail-body">
-          <h2 class="detail-name">{{ store.selectedRestaurant.name }}</h2>
-          <p>{{ store.selectedRestaurant.rating }}</p>
-          <p>{{ store.selectedRestaurant.address }}</p>
+            </li>
+          </ol>
         </div>
       </div>
     </div>
 
-    <button class="toggle-aside" @click="toggleAside()">
+    <button
+      v-if="!store.selectedRestaurant"
+      class="toggle-aside"
+      @click="toggleAside()"
+    >
       <i
         :class="
           isExpanded ? 'fa-solid fa-caret-left' : 'fa-solid fa-caret-right'
         "
       ></i>
     </button>
+
+    <div v-if="store.selectedRestaurant" class="detail-container">
+      <button @click="store.clearSelectedRestaurant()">
+        <i class="fa-solid fa-close"></i>
+      </button>
+
+      <img
+        :src="store.selectedRestaurant.image"
+        alt="餐廳圖片"
+        class="detail-res-image"
+      />
+      <div class="detail-body">
+        <h2 class="detail-name">{{ store.selectedRestaurant.name }}</h2>
+        <p>{{ store.selectedRestaurant.rating }}</p>
+        <p>{{ store.selectedRestaurant.address }}</p>
+      </div>
+    </div>
   </aside>
 </template>
 
 <style scoped>
+h2,
+h3,
+p {
+  margin: 0;
+  padding: 0;
+}
+
 .aside {
   background-color: var(--color-beige-50);
   width: 0;
   position: relative;
   transition: width 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+  position: relative;
+}
+
+.scroll-container {
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  scrollbar-color: var(--color-orange-600) var(--color-beige-50);
 }
 
 .aside-expanded {
@@ -121,12 +148,14 @@ const toggleAside = () => {
   top: calc((100% - 3rem) / 2);
 }
 
-.toggle-aside {
-  position: absolute;
-}
-
 .aside-content {
-  padding: 1.5rem;
+  padding: 0 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
 
   opacity: 0;
   transform: translateX(-20px);
@@ -141,6 +170,16 @@ const toggleAside = () => {
 }
 
 .aside-row {
+  width: 100%;
+  position: sticky;
+
+  padding: 1rem 0;
+  top: 0rem;
+  background-color: var(--color-beige-50);
+  z-index: 10;
+}
+
+.row-wrapper {
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -181,5 +220,67 @@ const toggleAside = () => {
 
 .advance-search:hover {
   background-color: var(--color-beige-300);
+}
+
+.res-list ol {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.list-card {
+  display: flex;
+  height: 12rem;
+  background-color: white;
+  border-radius: 0.5rem;
+  overflow: hidden;
+}
+
+.card-img {
+  width: 12rem;
+  height: 100%;
+  object-fit: cover;
+}
+
+.card-info {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  gap: 0.5rem;
+
+  padding: 0.5rem;
+}
+
+.detail-container {
+  width: 24rem;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 0;
+  right: -24.5rem;
+
+  background-color: var(--color-beige-50);
+}
+
+.detail-container button {
+  background-color: var(--color-beige-300);
+  border: none;
+  color: var(--color-dbrown-300);
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
 }
 </style>
